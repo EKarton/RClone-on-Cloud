9a7851bdc996b4cfeb8246e1f343dd8d67202982
Emilio K <e.kartonoe@gmail.com>
2026-08-02 17:09:57 -0700

    fix: remove redundant mkdir function

--- apps/web-ui/src/app/content-page/services/web-api/web-api.service.ts
+++ apps/web-ui/src/app/content-page/services/web-api/web-api.service.ts
@@ -51,16 +51,6 @@ export class WebApiService {
     );
   }
 
-  /** Creates a folder on a remote */
-  mkdir(remote: string, dirPath: string): Observable<Result<void>> {
-    const url = `${environment.webApiEndpoint}/api/v1/rclone/operations/mkdir`;
-    const requestBody = {
-      fs: `${remote}:`,
-      remote: dirPath,
-    };
-    return this.post<void>(url, requestBody);
-  }
-
   /** Creates a folder on a remote asynchronously */
   mkdirAsync(remote: string, dirPath: string): Observable<Result<AsyncJobResponse>> {
     const url = `${environment.webApiEndpoint}/api/v1/rclone/operations/mkdir`;
